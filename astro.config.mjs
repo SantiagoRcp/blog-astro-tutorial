5d14b0aa22f3fdb46a3e68f63943a1936ad800e7
SantiagoRcp <santiagorcp.9854@gmail.com>
2024-06-14 11:43:22 -0600

    modifying files and adding islands

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from "astro/config";
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://blog-astro-tutorialsrc.netlify.app/",
   prefetch: true,
+  integrations: [preact()]
 });
